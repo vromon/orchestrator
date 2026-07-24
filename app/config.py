618589d0de34e9fe8732230a63f9a73873050be0
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2026-07-24 15:33:21 +0000

    Test Config

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
-    PINECONE_API: str = ""
-    GEMINI_API: str = ""
-    TRIP_INDEX: str = ""
-    DB_CONNECTION: str = ""
+    PINECONE_API: str | None = None
+    GEMINI_API: str | None = None
+    TRIP_INDEX: str | None = None
+    DB_CONNECTION: str | None = None
 
 settings = Settings()
