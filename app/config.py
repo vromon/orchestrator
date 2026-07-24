f25a87e263503a8211b9950e25058a1807034f63
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2026-07-24 14:31:47 +0000

    Add default value

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 class Settings(BaseSettings):
-    model_config=SettingsConfigDict(env_file=".env",extra="ignore")
-    PINECONE_API:str
-    GEMINI_API:str
-    TRIP_INDEX:str
-    DB_CONNECTION:str
-settings=Settings()
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    
+    PINECONE_API: str = ""
+    GEMINI_API: str = ""
+    TRIP_INDEX: str = ""
+    DB_CONNECTION: str = ""
+
+settings = Settings()
