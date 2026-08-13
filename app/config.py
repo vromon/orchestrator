b8cd3fa24fb4bdd222c5c517850000ca42a5de47
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-08-13 17:51:52 +0600

    feat(config): add UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN to Settings

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-<<<<<<< HEAD
     model_config=SettingsConfigDict(env_file=".env",extra="ignore")
     PINECONE_API:str
     GEMINI_API:str
@@ -10,13 +9,3 @@ class Settings(BaseSettings):
     UPSTASH_REDIS_REST_URL:str
     UPSTASH_REDIS_REST_TOKEN:str
 settings=Settings()
-=======
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-    
-    PINECONE_API: str | None = None
-    GEMINI_API: str | None = None
-    TRIP_INDEX: str | None = None
-    DB_CONNECTION: str | None = None
-
-settings = Settings()
->>>>>>> 17a7a6ffcd8cedeb436a351cc5c6880743e6cfea
