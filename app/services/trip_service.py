600dc3629ebc67bbb6fddfdf733c98f3dd311c1c
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-08-17 16:53:14 +0600

    fix:add circuit breaker in redis integration to prevent failed response

--- app/services/trip_service.py
+++ app/services/trip_service.py
@@ -13,9 +13,12 @@ import json
 import time
 import asyncio
 import hashlib
-
+import pybreaker
+from app.utils.circuite_breaker import get_circuit_breaker
 redis_client=get_redis_cient()
+circuit_breaker=get_circuit_breaker()
 async def stream_trip(trip_request: TripSchema):
+
     global g_uncached_time
     generalised=trip_request.user_query.lower().strip()
     cache_key=hashlib.sha256(
@@ -23,22 +26,34 @@ async def stream_trip(trip_request: TripSchema):
     ).hexdigest()
     
     start_time=time.perf_counter()
-    cached = await redis_client.get(cache_key)
-    if cached:
-        cached_res = json.loads(cached)
-        words=cached_res.split(".")
+    try:
+        cached = await circuit_breaker.call_async(redis_client.get,cache_key)
+    except pybreaker.CircuitBreakerError:
+        print("Circuit is Open!")
+    except Exception as e:
+        print(f"Error getting redis data:{e}")
+    
+    try:
+        if cached:
 
-        for i,word in enumerate(words):
-            chunk=word+" "if i<len(words)-1 else word
-            yield chunk
-            await asyncio.sleep(0.01)
+            cached_res = json.loads(cached)
+            words=cached_res.split(".")
+
+            for i,word in enumerate(words):
+
+                chunk=word+" "if i<len(words)-1 else word
+                yield chunk
+                await asyncio.sleep(0.01)
  
-        cached_time = time.perf_counter() - start_time
-        print(f"Time spent at cache hit={cached_time:.4f} seconds")
-        time_reduced=g_uncached_time-cached_time
-        percentage_reduction=(time_reduced/g_uncached_time)*100
-        print(f"Response time reduced by {percentage_reduction:.2f}%")
-        return 
+            cached_time = time.perf_counter() - start_time
+            print(f"Time spent at cache hit={cached_time:.4f} seconds")
+            time_reduced=g_uncached_time-cached_time
+            percentage_reduction=(time_reduced/g_uncached_time)*100
+            print(f"Response time reduced by {percentage_reduction:.2f}%")
+            return 
+    except Exception as e:
+        print(f"Error parsing cached JSON: {e}")
+         
 
     
     cached_res=""
@@ -47,11 +62,21 @@ async def stream_trip(trip_request: TripSchema):
             cached_res+=chunk
             yield chunk
         
+    try:
+
+        await circuit_breaker.call_async(redis_client.setex,cache_key,20,json.dumps(cached_res))
+    
+        uncached_time = time.perf_counter() - start_time
+        g_uncached_time=uncached_time
+        print(f"Time spent at cache miss={uncached_time:.4f} seconds")
+    except pybreaker.CircuitBreakerError:
+        print("Circuit is OPEN! Skipping Redis SET.")
+    except Exception as e:
+            print(f"Error saving data to redis: {e}")
+    
         
-    await redis_client.setex(cache_key,20,json.dumps(cached_res))
-    uncached_time = time.perf_counter() - start_time
-    g_uncached_time=uncached_time
-    print(f"Time spent at cache miss={uncached_time:.4f} seconds")
+
+       
         
 
 supabase=get_supabase_client()
