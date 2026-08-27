4478062108b0ce762c15787e0421d2a381b900f3
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-08-27 19:50:17 +0600

    feat:add pinecone namespace

--- app/ai/vectorstore.py
+++ app/ai/vectorstore.py
@@ -8,5 +8,6 @@ def get_vectorstore() -> PineconeVectorStore:
     return PineconeVectorStore(
         index_name=settings.TRIP_INDEX,
         embedding=embedding_function,
-        pinecone_api_key=settings.PINECONE_API
+        pinecone_api_key=settings.PINECONE_API,
+        namespace="aaa"
     )
